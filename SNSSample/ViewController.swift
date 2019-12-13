a80abbd7ff4245ce42ce9a37b94eb13af2fc157c
権藤　俊 <gondo.syun0711@gmail.com>
2019-12-13 20:25:48 +0900

    Initial Commit

---
+++ SNSSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNSSample
+//
+//  Created by しゅん on 2019/12/13.
+//  Copyright © 2019 g-chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
